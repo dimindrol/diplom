393372faac29a10779e35bf5ef2f01054ca63d59
dimindrol <dimindrol_95@mail.ru>
2024-08-24 08:22:38 +0000

    Уменьшена подсеть

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -9,7 +9,7 @@ resource "yandex_vpc_subnet" "public_subnet" {
   name           = "public-subnet"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.my_vpc.id
-  v4_cidr_blocks = ["10.0.1.0/27"]
+  v4_cidr_blocks = ["10.0.1.0/28"]
 }
 
 # Публичная подсеть для Zabbix и Kibana
@@ -17,7 +17,7 @@ resource "yandex_vpc_subnet" "public_services_subnet" {
   name           = "public-services-subnet"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.my_vpc.id
-  v4_cidr_blocks = ["10.0.1.32/27"]
+  v4_cidr_blocks = ["10.0.1.16/28"]
 }
 
 # Приватная подсеть для web-серверов
@@ -26,7 +26,7 @@ resource "yandex_vpc_subnet" "web_subnet_a" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.my_vpc.id
   route_table_id = yandex_vpc_route_table.rt.id
-  v4_cidr_blocks = ["10.0.2.0/27"]
+  v4_cidr_blocks = ["10.0.2.0/28"]
 }
 
 # Приватная подсеть для web-серверов
@@ -35,7 +35,7 @@ resource "yandex_vpc_subnet" "web_subnet_b" {
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.my_vpc.id
   route_table_id = yandex_vpc_route_table.rt.id
-  v4_cidr_blocks = ["10.0.2.32/27"]
+  v4_cidr_blocks = ["10.0.2.16/28"]
 }
 
 # Приватная подсеть для Elasticsearch
@@ -44,7 +44,7 @@ resource "yandex_vpc_subnet" "elasticsearch_subnet" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.my_vpc.id
   route_table_id = yandex_vpc_route_table.rt.id
-  v4_cidr_blocks = ["10.0.2.64/27"]
+  v4_cidr_blocks = ["10.0.2.32/28"]
 }
 
 
